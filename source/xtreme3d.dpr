f0a0b0d71b3ec230d3b729a86b399599beb58679
Timur Gafarov <gecko0307@gmail.com>
2016-04-25 12:29:20 +0300

    Added Navigator and FPSManager functions

--- source/xtreme3d.dpr
+++ source/xtreme3d.dpr
@@ -13,7 +13,8 @@ uses
   GLBumpShader, GLCelShader, GLContext, GLTerrainRenderer, GLHeightData,
   GLBlur, GLSLShader, GLMultiMaterialShader, GLOutlineShader, GLHiddenLineShader,
   ApplicationFileIO, GLMaterialScript, GLWaterPlane, GeometryBB, GLExplosionFx,
-  GLSkyBox, GLShadowPlane, GLShadowVolume, GLSkydome, GLLensFlare, GLDCE;
+  GLSkyBox, GLShadowPlane, GLShadowVolume, GLSkydome, GLLensFlare, GLDCE,
+  GLNavigator, GLFPSMovement;
 
 type
    TEmpty = class(TComponent)
@@ -76,10 +77,144 @@ end;
 {$I 'shadowvolume'}
 {$I 'skydome'}
 {$I 'water'}
+{$I 'navigator'}
 {$I 'dce'}
 {$I 'text'}
 
+function FpsManagerCreate: real; stdcall;
+var
+  FPSManager: TGLFPSMovementManager;
+begin
+  FPSManager := TGLFPSMovementManager.Create(scene);
+  Result := Integer(FPSManager);
+end;
+
+function FpsManagerSetNavigator(man, nav: real): real; stdcall;
+begin
+  TGLFPSMovementManager(trunc64(man)).Navigator := TGLNavigator(trunc64(nav));
+  Result := 1;
+end;
+
+function FpsManagerSetMovementScale(man, scale: real): real; stdcall;
+begin
+  TGLFPSMovementManager(trunc64(man)).MovementScale := scale;
+  Result := 1;
+end;
+
+function FpsManagerAddMap(man, ffm: real): real; stdcall;
+begin
+  TGLFPSMovementManager(trunc64(man)).Maps.addMap(TGLFreeform(trunc64(ffm)));
+  Result := 1;
+end;
+
+function FpsManagerRemoveMap(man, ffm: real): real; stdcall;
+begin
+  TGLFPSMovementManager(trunc64(man)).Maps.findMap(TGLFreeform(trunc64(ffm))).Free;
+  Result := 1;
+end;
+
+function FpsManagerMapSetCollisionGroup(man, ffm, group: real): real; stdcall;
+begin
+  TGLFPSMovementManager(trunc64(man)).Maps.findMap(TGLFreeform(trunc64(ffm))).CollisionGroup := trunc64(group);
+  Result := 1;
+end;
+
+function FpsSetManager(obj, man: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  fps.Manager := TGLFPSMovementManager(trunc64(man));
+  Result := 1;
+end;
+
+function FpsSetCollisionGroup(obj, group: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  fps.CollisionGroup := trunc64(group);
+  Result := 1;
+end;
+
+function FpsSetSphereRadius(obj, radius: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  fps.SphereRadius := radius;
+  Result := 1;
+end;
+
+function FpsSetGravity(obj, mode: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  fps.GravityEnabled := Boolean(trunc64(mode));
+  Result := 1;
+end;
+
+function FpsMove(obj, spd: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  fps.MoveForward(spd);
+  Result := 1;
+end;
+
+function FpsStrafe(obj, spd: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  fps.StrafeHorizontal(spd);
+  Result := 1;
+end;
+
+function FpsLift(obj, spd: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  fps.StrafeVertical(spd);
+  Result := 1;
+end;
+
+function FpsGetVelocity(obj, ind: real): real; stdcall;
+var
+  ob: TGLBaseSceneObject;
+  fps: TGLBFPSMovement;
+begin
+  ob := TGLBaseSceneObject(trunc64(obj));
+  fps := GetOrCreateFPSMovement(ob);
+  Result := fps.velocity[trunc64(ind)];
+end;
+
+// TODO:
+// FpsCountCollisions
+// FpsClearCollisions
+// FpsGetCollisionPosition
+// FpsGetCollisionNormal
+// FpsGetCollidedObject
+
 exports
+
 //Engine
 EngineCreate, EngineDestroy, EngineSetObjectsSorting, EngineSetCulling,
 SetPakArchive,
@@ -268,6 +403,12 @@ ShadowvolumeCreate, ShadowvolumeSetActive,
 ShadowvolumeAddLight, ShadowvolumeRemoveLight,
 ShadowvolumeAddOccluder, ShadowvolumeRemoveOccluder,
 ShadowvolumeSetDarkeningColor, ShadowvolumeSetMode, ShadowvolumeSetOptions,
+//Navigator
+NavigatorCreate, NavigatorSetObject, NavigatorSetUseVirtualUp, NavigatorSetVirtualUp,  
+NavigatorTurnHorizontal, NavigatorTurnVertical, NavigatorMoveForward,
+NavigatorStrafeHorizontal, NavigatorStrafeVertical, NavigatorStraighten,
+NavigatorFlyForward, NavigatorMoveUpWhenMovingForward, NavigatorInvertHorizontalWhenUpsideDown,
+NavigatorSetAngleLock, NavigatorSetAngles,
 //DCE
 DceManagerCreate, DceManagerStep, DceManagerSetGravity, DceManagerSetWorldDirection,
 DceManagerSetWorldScale, DceManagerSetMovementScale,
@@ -283,6 +424,11 @@ DceDynamicInGround, DceDynamicSetMaxRecursionDepth,
 DceStaticSetManager, DceStaticSetActive, DceStaticSetShape, DceStaticSetLayer,
 DceStaticSetSize, DceStaticSetSolid, DceStaticSetFriction, DceStaticSetBounceFactor,
 DceDynamicGetVelocity,
+//FPSManager
+FpsManagerCreate, FpsManagerSetNavigator, FpsManagerSetMovementScale,
+FpsManagerAddMap, FpsManagerRemoveMap, FpsManagerMapSetCollisionGroup,
+FpsSetManager, FpsSetCollisionGroup, FpsSetSphereRadius, FpsSetGravity,
+FpsMove, FpsStrafe, FpsLift, FpsGetVelocity,
 //Text
 TextRead;
 
