044aa1aa7a2a815770db6937993185fee51599e2
Timur Gafarov <gecko0307@gmail.com>
2016-05-10 18:10:01 +0300

    More sea-like water shader

--- gm/data/water-vp-2.glsl
+++ gm/data/water-vp-2.glsl
@@ -1,6 +1,9 @@
 #version 120
 varying vec3 position;
 varying vec3 normal;
+varying vec3 tangent;
+varying vec3 binormal;
+varying vec3 eyeTan;
 varying vec3 eye;
 varying vec3 worldView;
 
@@ -20,6 +23,14 @@ void main()
     worldView = normalize(worldPos - worldCamPos); // vector from camera to vertex
    
     normal = normalize(gl_NormalMatrix * gl_Normal);
+    tangent = normalize(gl_NormalMatrix * vec3(1, 0, 0));
+    binormal = cross(normal, tangent);
+    
+    eyeTan = position;
+    eyeTan.x = dot(position, tangent);
+    eyeTan.y = dot(position, binormal);
+    eyeTan.z = dot(position, normal);
+    eyeTan = -eyeTan;
    
     gl_Position = ftransform();
 }
