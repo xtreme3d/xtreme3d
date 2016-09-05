651280cbe0bb63f4109b59dd52ce26db79050d4b
Timur Gafarov <gecko0307@gmail.com>
2016-09-05 10:30:49 +0300

    Added ASE format support

--- source/xtreme3d.dpr
+++ source/xtreme3d.dpr
@@ -9,7 +9,7 @@ uses
   GLPolyhedron, GLTeapot, GLzBuffer, GLFile3DS, GLFileGTS, GLFileLWO, GLFileMD2,
   GLFileMD3, Q3MD3, GLFileMS3D, GLFileMD5, GLFileNMF, GLFileNurbs, GLFileObj, GLFileOCT,
   GLFilePLY, GLFileQ3BSP, GLFileSMD, GLFileSTL, GLFileTIN, GLFileB3D,
-  GLFileMDC, GLFileVRML, GLFileLOD, GLFileX, GLFileCSM, GLFileLMTS,
+  GLFileMDC, GLFileVRML, GLFileLOD, GLFileX, GLFileCSM, GLFileLMTS, GLFileASE,
   GLPhongShader, VectorLists, GLThorFX, GLFireFX,
   GLTexCombineShader, GLBumpShader, GLCelShader, GLContext, GLTerrainRenderer, GLHeightData,
   GLBlur, GLSLShader, GLMultiMaterialShader, GLOutlineShader, GLHiddenLineShader,
