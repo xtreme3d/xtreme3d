01a4ad2d4fd7312a2c49a5b84ec0404b49029380
Timur Gafarov <gecko0307@gmail.com>
2017-07-07 15:04:44 +0300

    Added MakeColorRGB and MakeColorRGBFloat

--- source/xtreme3d.dpr
+++ source/xtreme3d.dpr
@@ -584,6 +584,26 @@ begin
   result := 1.0;
 end;
 
+function MakeColorRGB(r, g, b: real): real; stdcall;
+var
+  rb, gb, bb: Byte;
+begin
+  rb := trunc64(r);
+  gb := trunc64(g);
+  bb := trunc64(b);
+  result := Integer(RGB(rb, gb, bb));
+end;
+
+function MakeColorRGBFloat(r, g, b: real): real; stdcall;
+var
+  rb, gb, bb: Byte;
+begin
+  rb := trunc64(r * 255.0);
+  gb := trunc64(g * 255.0);
+  bb := trunc64(b * 255.0);
+  result := Integer(RGB(rb, gb, bb));
+end;
+
 {$I 'xtreme3d/lua/engine'}
 {$I 'xtreme3d/lua/viewer'}
 {$I 'xtreme3d/lua/dummycube'}
@@ -1419,6 +1439,8 @@ OdeDynamicSetPosition, OdeDynamicSetRotationQuaternion,
 // Input
 MouseSetPosition, MouseGetPositionX, MouseGetPositionY, MouseShowCursor, 
 KeyIsPressed,
+// Color
+MakeColorRGB, MakeColorRGBFloat,
 // Window
 WindowCreate, WindowGetHandle, WindowSetTitle, WindowDestroy,
 // Squall
