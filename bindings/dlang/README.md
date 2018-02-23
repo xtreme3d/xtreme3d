23cded58232c6f113763169ed703ecf82ce506b3
Timur Gafarov <gecko0307@gmail.com>
2018-02-23 14:20:25 +0300

    Updated script

--- bindings/dlang/README.md
+++ bindings/dlang/README.md
@@ -1,5 +1,13 @@
 Xtreme3D for D
 ==============
-This is Derelict D binding for Xtreme3D engine: https://github.com/xtreme3d/xtreme3d. It is is automatically generated from Xtreme3D source code using genbinding.py script. 
+This is Derelict D binding for Xtreme3D engine. It can automatically generated from Xtreme3D source code using genbinding.py script. 
 
 The demo uses SDL2 to create a window and handle user input.
+
+Usage
+-----
+If you have Python installed, just run `genbinding.py` script. 
+
+Assuming Xtreme3D repository structure unchanged, it will analyze Xtreme3D source code and generate two files: `derelict/xtreme3d/xtreme3d_function_headers.txt` and `derelict/xtreme3d/xtreme3d_function_bindings.txt`, which are statically included in Derelict binding. We update these files after API changes, so it's actually not necessary to run the script if you are using release version of the source code. 
+
+Then you can use Dub to compile the demo application: `dub build`. Do not forget to copy `xtreme3d.dll` (and also `ode.dll`, `freetype.dl` if needed) to application directory before running.
--- bindings/dlang/genbinding.py
+++ bindings/dlang/genbinding.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import sys
 import os
 import re
@@ -5,7 +7,27 @@ import re
 declStrings = []
 bindStrings = []
 
-directory = sys.argv[1]
+x3d_path = '../../source/xtreme3d'
+output_path = 'derelict/xtreme3d'
+
+directory = ''
+if len(sys.argv) < 2:
+    directory = x3d_path
+else:
+    directory = sys.argv[1]
+
+if not os.path.exists(directory):
+    sys.exit('Error: input directory \"' + directory + '\" does not exist')
+
+outputDirectory = ''
+if len(sys.argv) < 3:
+    outputDirectory = output_path
+else:
+    outputDirectory = sys.argv[2]
+
+if not os.path.exists(outputDirectory):
+    sys.exit('Error: output directory \"' + outputDirectory + '\" does not exist')
+
 for filename in os.listdir(directory):
     path = directory + '/' + filename
     if os.path.isdir(path):
@@ -52,11 +74,12 @@ for filename in os.listdir(directory):
                 bindStrings.append(bindStr)
     declStrings.append("")
     bindStrings.append("")
-                
+
+headersFile = open(outputDirectory + '/xtreme3d_function_headers.txt', 'w')
 for s in declStrings:
-    print s
-        
-print " "
-            
+    headersFile.write("%s\n" % s)
+
+bindingsFile = open(outputDirectory + '/xtreme3d_function_bindings.txt', 'w')
 for s in bindStrings:
-    print s
+    bindingsFile.write("%s\n" % s)
+
