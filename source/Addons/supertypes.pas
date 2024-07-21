db5d3a22cd3126c252272defaeef324d68d28a39
Timur Gafarov <gecko0307@gmail.com>
2024-07-22 02:30:40 +0300

    Fix

--- source/Addons/supertypes.pas
+++ source/Addons/supertypes.pas
@@ -21,9 +21,32 @@ PInt = PInt32;
 
 SInt32 = Int32;
 
-PtrInt = ^Int32;
-PtrUInt = ^Uint32;
+{$IFNDEF FPC}
+{$IFDEF CPUX64}
+  PtrInt = Int64;
+  PtrUInt = UInt64;
+{$ELSE}
+  PtrInt = longint;
+  PtrUInt = Longword;
+{$ENDIF}
+{$ENDIF}
+  SuperInt = Int64;
 
+{$if (sizeof(Char) = 1)}
+  SOChar = WideChar;
+  SOIChar = Word;
+  PSOChar = PWideChar;
+{$IFDEF FPC}
+  SOString = UnicodeString;
+{$ELSE}
+  SOString = WideString;
+{$ENDIF}
+{$else}
+  SOChar = Char;
+  SOIChar = Word;
+  PSOChar = PChar;
+  SOString = string;
+{$ifend}
 implementation
 
 end.
