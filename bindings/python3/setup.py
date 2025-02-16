658fd52b702521d4d5cf863a16fb04d21b727961
Timur Gafarov <gecko0307@gmail.com>
2025-02-16 20:18:27 +0300

    Optimize build

--- bindings/python3/setup.py
+++ bindings/python3/setup.py
@@ -5,18 +5,33 @@ from distutils.sysconfig import get_python_lib
 from cx_Freeze import setup, Executable
 
 base = None
-if (sys.platform == "win32"):
-    base = "Win32GUI"
+if (sys.platform == 'win32'):
+    base = 'Win32GUI'
+
+mainScript = 'test.py'
+
+if '--script' in sys.argv:
+    index = sys.argv.index('--script')
+    sys.argv.pop(index)
+    mainScript = sys.argv.pop(index)
+
+executable = Executable(script = mainScript, icon = 'icon.ico', base = base)
+
+exclude = ['tkinter', 'tcl', 'tk', 'sqlite3', 'xml', 'xmlrpc', 'http', 'html', 'urllib', 'email', 'unittest', 'xtreme3d.dll', 'ode64s.dll', 'SDL2.dll', 'SDL2_ttf.dll']
+include = ['data', 'xtreme3d.dll', 'ode64s.dll', 'SDL2.dll', 'SDL2_ttf.dll']
 
 setup(
-    name = 'xreme3d',
-    version = '0.0.1',
-    description = 'Xtreme3D',
+    name = 'Xtreme3D SDK Demo',
+    version = '1.0.0',
+    description = 'Xtreme3D SDK Demo',
     options = {
         'build_exe': {
-        'packages': ['os', 'sys', 'ctypes', 'json'],
-        'include_files': ['data', 'xtreme3d.dll', 'ode64s.dll', 'SDL2.dll', 'SDL2_ttf.dll'],
-        'include_msvcr': True,
-    }},
-    executables = [Executable(script='test.py', icon='icon.ico', base=base)]
+            'packages': ['os', 'sys', 'ctypes', 'json'],
+            'excludes': exclude,
+            'include_files': include,
+            'include_msvcr': True,
+            'optimize': 2
+        }
+    },
+    executables = [executable]
 )
