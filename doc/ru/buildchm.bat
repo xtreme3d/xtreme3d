ef622bfbbba16fd60523f98f3e7124cc15f9ce98
Timur Gafarov <gecko0307@gmail.com>
2025-03-19 14:58:25 +0300

    New documentation directory structure to support multiple languages

--- doc/ru/buildchm.bat
+++
@@ -1,1 +0,0 @@
-chmbuilder html xtreme3d.html html/TOC.hhc html/Index.hhk help-ru.chm $0419
--- doc/ru/buildhtml.js
+++ doc/ru/buildhtml.js
@@ -19,18 +19,20 @@ function generatePage(title, markdown, language, encoding="utf8")
     return Mustache.render(pageTemplate, data);
 }
 
+const lang = "ru";
+
 const functions =
 [
-    { title: "Engine", inFilename: "markdown/functions/engine.md", outFilename: "html/functions/engine.html" },
-    { title: "Pak", inFilename: "markdown/functions/pak.md", outFilename: "html/functions/pak.html" },
-    { title: "Viewer", inFilename: "markdown/functions/viewer.md", outFilename: "html/functions/viewer.html" },
-    { title: "PickList", inFilename: "markdown/functions/picklist.md", outFilename: "html/functions/picklist.html" },
-    { title: "Dummycube", inFilename: "markdown/functions/dummycube.md", outFilename: "html/functions/dummycube.html" },
-    { title: "Camera", inFilename: "markdown/functions/camera.md", outFilename: "html/functions/camera.html" },
-    { title: "Light", inFilename: "markdown/functions/light.md", outFilename: "html/functions/light.html" },
-    { title: "Font and Text", inFilename: "markdown/functions/fonttext.md", outFilename: "html/functions/fonttext.html" },
-    { title: "Sprite", inFilename: "markdown/functions/sprite.md", outFilename: "html/functions/sprite.html" },
-    { title: "HUDShape", inFilename: "markdown/functions/hudshape.md", outFilename: "html/functions/hudshape.html" }
+    { title: "Engine", inFilename: `markdown/${lang}/functions/engine.md`, outFilename: `${lang}/html/functions/engine.html` },
+    { title: "Pak", inFilename: `markdown/${lang}/functions/pak.md`, outFilename: `${lang}/html/functions/pak.html` },
+    { title: "Viewer", inFilename: `markdown/${lang}/functions/viewer.md`, outFilename: `${lang}/html/functions/viewer.html` },
+    { title: "PickList", inFilename: `markdown/${lang}/functions/picklist.md`, outFilename: `${lang}/html/functions/picklist.html` },
+    { title: "Dummycube", inFilename: `markdown/${lang}/functions/dummycube.md`, outFilename: `${lang}/html/functions/dummycube.html` },
+    { title: "Camera", inFilename: `markdown/${lang}/functions/camera.md`, outFilename: `${lang}/html/functions/camera.html` },
+    { title: "Light", inFilename: `markdown/${lang}/functions/light.md`, outFilename: `${lang}/html/functions/light.html` },
+    { title: "Font and Text", inFilename: `markdown/${lang}/functions/fonttext.md`, outFilename: `${lang}/html/functions/fonttext.html` },
+    { title: "Sprite", inFilename: `markdown/${lang}/functions/sprite.md`, outFilename: `${lang}/html/functions/sprite.html` },
+    { title: "HUDShape", inFilename: `markdown/${lang}/functions/hudshape.md`, outFilename: `${lang}/html/functions/hudshape.html` }
 ];
 
 const outputEncoding = "win1251";
@@ -39,7 +41,7 @@ const metaEncoding = "windows-1251";
 for (const f of functions)
 {
     const markdown = fs.readFileSync(f.inFilename, "utf8");
-    const output = generatePage(f.title, markdown, "ru", metaEncoding);
+    const output = generatePage(f.title, markdown, lang, metaEncoding);
     const encodedData = iconv.encode(output, outputEncoding);
     fs.writeFileSync(f.outFilename, encodedData);
 }
